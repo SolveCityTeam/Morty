21e76294728e265450dc3bad855f1e24449c88c7
Sean Marquez <capsulecorplab@gmail.com>
2017-04-27 15:17:53 -0700

    example for querying pubmed using PubmedID

---
+++ pubmedquery.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+from pubmed_lookup import PubMedLookup
+from pubmed_lookup import Publication
+
+def search(PubMedID):
+	# Retrieve a PubMed record:
+	# NCBI will contact user by email if excessive queries are detected
+	"""
+	Retrieve a PubMed record using its PubMed ID or PubMed URL.
+	(e.g., '22331878' or 'http://www.ncbi.nlm.nih.gov/pubmed/22331878')
+	"""
+	email = ''
+	#url = 'http://www.ncbi.nlm.nih.gov/pubmed/22331878'
+	#lookup = PubMedLookup(url, email)
+	lookup = PubMedLookup(PubMedID,email)
+
+	# Create a Publication object:
+	publication = Publication(lookup)    # Use 'resolve_doi=False' to keep DOI URL
+
+	# Access the Publication object's attributes:
+	tupleoutput = (
+	"""
+	TITLE:\n{title}\n
+	AUTHORS:\n{authors}\n
+	JOURNAL:\n{journal}\n
+	YEAR:\n{year}\n
+	MONTH:\n{month}\n
+	DAY:\n{day}\n
+	URL:\n{url}\n
+	PUBMED:\n{pubmed}\n
+	CITATION:\n{citation}\n
+	MINICITATION:\n{mini_citation}\n
+	ABSTRACT:\n{abstract}\n
+	"""
+	.format(**{
+	  'title': publication.title,
+	  'authors': publication.authors,
+	  'journal': publication.journal,
+	  'year': publication.year,
+	  'month': publication.month,
+	  'day': publication.day,
+	  'url': publication.url,
+	  'pubmed': publication.pubmed_url,
+	  'citation': publication.cite(),
+	  'mini_citation': publication.cite_mini(),
+	  'abstract': repr(publication.abstract),
+	}))
+	
+	return tupleoutput
+	
+#print(search('','ewchehab@rice.edu'))
+print(search(22331878))
+
