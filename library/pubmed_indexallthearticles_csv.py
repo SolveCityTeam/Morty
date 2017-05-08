90edefae46b84147e716231eebdb0e82e0b4f170
Sean Marquez <capsulecorplab@gmail.com>
2017-05-08 10:08:31 -0700

    indexer for 'all the articles'

---
+++ library/pubmed_indexallthearticles_csv.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+"""
+Created on Fri Apr 28 11:30:56 2017
+@author: Dr. Briefs
+
+@Dependencies: Requires 'PubMedLookup' Python module from https://github.com/mfcovington/pubmed-lookup
+"""
+
+from pubmed_lookup import PubMedLookup
+from pubmed_lookup import Publication
+import pandas as pd
+import os
+
+def search(PubMedID):
+	# Retrieve a PubMed record:
+	# NCBI will contact user by email if excessive queries are detected
+	"""
+	Retrieve a PubMed record using its PubMed ID or PubMed URL.
+	(e.g., '22331878' or 'http://www.ncbi.nlm.nih.gov/pubmed/22331878')
+	"""
+	email = ''
+	#url = 'http://www.ncbi.nlm.nih.gov/pubmed/22331878'
+	#lookup = PubMedLookup(url, email)
+	#print(type(PubMedID))
+	lookup = PubMedLookup(PubMedID,email)
+
+	# Create a Publication object:
+	publication = Publication(lookup)    # Use 'resolve_doi=False' to keep DOI URL
+
+	# Access the Publication object's attributes:
+	tupleoutput = [(
+		PubMedID,
+		publication.title,
+		publication.authors,
+		publication.journal,
+		publication.year,
+		publication.month,
+		publication.day,
+		publication.url,
+		publication.pubmed_url,
+		publication.cite(),
+		publication.cite_mini(),
+		repr(publication.abstract),
+		0,
+	)]
+	
+	return tupleoutput
+	
+#print(search('','ewchehab@rice.edu'))
+#print(search(22331878))
+
+for id_no in range(10000001,28470030):
+	#print(tuple(id_no))
+	df = pd.DataFrame(search(id_no), columns=['PubMedID','title','authors','journal','year','month','day','url','pubmed','citation','mini_citation','abstract','pagerank'])
+
+	# if file does not exist write header 
+	if not os.path.isfile('pubmed_index.csv'):
+		print('Parsing id_no #',id_no,'...'),
+		df.to_csv('pubmed_index.csv',header ='column_names')
+	elif pd.read_csv('pubmed_index.csv')['PubMedID'].max() >= id_no:
+		print('repeat no:',id_no)
+	else: # else it exists so append without writing the header
+		print('Parsing id_no #',id_no,'...'),
+		df.to_csv('pubmed_index.csv',mode = 'a',header=False)
+
+
